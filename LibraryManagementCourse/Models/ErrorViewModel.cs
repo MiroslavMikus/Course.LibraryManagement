d3cc79d4ecae33e012ee00bdfd0379820a515ef6
Mikus Miroslav <miroslav.mikus@outlook.com>
2017-11-27 05:37:50 +0100

    Add project files.

---
+++ LibraryManagementCourse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibraryManagementCourse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
