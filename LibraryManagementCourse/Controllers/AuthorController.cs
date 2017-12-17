0ca884c64716cea85e910953fcb0c3ad66d0e86e
Mikus Miroslav <miroslav.mikus@outlook.com>
2017-12-17 17:31:20 +0100

    19# Author Controller

---
+++ LibraryManagementCourse/Controllers/AuthorController.cs
@@ -0,0 +1,69 @@
+﻿using LibraryManagementCourse.Data.Interfaces;
+using LibraryManagementCourse.Data.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementCourse.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorController(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        [Route("Author")]
+        public IActionResult List()
+        {
+            var authors = _authorRepository.GetAllWithBooks();
+
+            if (authors.Count() == 0) return View("Empty");
+
+            return View(authors);
+        }
+
+        public IActionResult Update(int id)
+        {
+            var author = _authorRepository.GetById(id);
+
+            if (author == null) return NotFound();
+
+            return View(author);
+        }
+
+        [HttpPost] 
+        public IActionResult Update (Author author)
+        {
+            _authorRepository.Update(author);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Author author)
+        {
+            _authorRepository.Create(author);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var author = _authorRepository.GetById(id);
+
+            _authorRepository.Delete(author);
+
+            return RedirectToAction("List");
+        }
+    }
+}
