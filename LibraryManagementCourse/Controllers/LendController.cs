aa6916884085cb6d1c8f9786ba1c3b4ee5e13296
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-01-09 20:33:34 +0100

    24# Lend Controller

---
+++ LibraryManagementCourse/Controllers/LendController.cs
@@ -0,0 +1,65 @@
+﻿using LibraryManagementCourse.Data.Interfaces;
+using LibraryManagementCourse.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementCourse.Controllers
+{
+    public class LendController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly ICustomerRepository _customerRepository;
+
+        public LendController(IBookRepository bookRepository, ICustomerRepository customerRepository)
+        {
+            _bookRepository = bookRepository;
+            _customerRepository = customerRepository;
+        }
+
+        public IActionResult List()
+        {
+            // load all available books
+            var availableBooks = _bookRepository.Find(x => x.BorrowerId == 0);
+            // check collection
+            if (availableBooks.Count() == 0)
+            {
+                return View(availableBooks);
+            }
+            else
+            {
+                return View("Empty");
+            }
+        }
+
+        public IActionResult LendBook(int bookId)
+        {
+            // load current book and all customers
+            var lendVM = new LendViewModel()
+            {
+                Book = _bookRepository.GetById(bookId),
+                Customers = _customerRepository.GetAll()
+            };
+            // Send data to the Lend view
+            return View(lendVM);
+        }
+
+        [HttpPost]
+        public IActionResult LendBook(LendViewModel lendViewModel)
+        {
+            // update the database 
+            var book = _bookRepository.GetById(lendViewModel.Book.BookId);
+
+            var customer = _customerRepository.GetById(lendViewModel.Book.BorrowerId);
+
+            book.Borrower = customer;
+
+            _bookRepository.Update(book);
+
+            // redirect to the list view
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ LibraryManagementCourse/ViewModel/LendViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagementCourse.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementCourse.ViewModel
+{
+    public class LendViewModel
+    {
+        public Book Book { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
+    }
+}
