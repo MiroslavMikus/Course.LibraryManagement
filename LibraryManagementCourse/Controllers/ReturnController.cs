3a9d027a78d3675f7fa64159bfde4cc848674ccb
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-01-15 21:19:58 +0100

    26# Return Controller

---
+++ LibraryManagementCourse/Controllers/ReturnController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementCourse.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementCourse.Controllers
+{
+    public class ReturnController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly ICustomerRepository _customerRepository;
+
+        public ReturnController(IBookRepository bookRepository, ICustomerRepository customerRepository)
+        {
+            _bookRepository = bookRepository;
+            _customerRepository = customerRepository;
+        }
+
+        public IActionResult List()
+        {
+            // load all borrowed books
+            var borrowedBooks = _bookRepository.FindWithAuthorAndBorrower(x => x.BorrowerId == 0);
+            // Check the books collection
+            if(borrowedBooks == null || borrowedBooks.ToList().Count() == 0)
+            {
+                return View("Empty");
+            }
+            return View(borrowedBooks);
+        }
+
+        public IActionResult ReturABook(int bookId)
+        {
+            // load the current book
+            var book = _bookRepository.GetById(bookId);
+            // remove borrower
+            book.Borrower = null;
+
+            book.BorrowerId = 0;
+            // update database
+            _bookRepository.Update(book);
+            // redirect to list method
+            return RedirectToAction("List");
+        }
+    }
+}
