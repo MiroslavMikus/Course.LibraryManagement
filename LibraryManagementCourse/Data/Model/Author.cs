ad85e8d001a88d0d9395199bc3ad7a7bb7e3f4ea
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-01-20 17:16:18 +0100

    30# Data model validation

--- LibraryManagementCourse/Data/Model/Author.cs
+++ LibraryManagementCourse/Data/Model/Author.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace LibraryManagementCourse.Data.Model
     public class Author
     {
         public int AuthorId { get; set; }
+
+        [Required, MinLength(3), MaxLength(50)]
         public string Name { get; set; }
 
         public virtual ICollection<Book> Books { get; set; }
--- LibraryManagementCourse/Data/Model/Book.cs
+++ LibraryManagementCourse/Data/Model/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace LibraryManagementCourse.Data.Model
     public class Book
     {
         public int BookId { get; set; }
+        [Required, MinLength(3), MaxLength(50)]
         public string Title { get; set; }
 
         public virtual Author Author { get; set; }
