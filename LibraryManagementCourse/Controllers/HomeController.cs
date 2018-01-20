d9b437370a9842f022bbae80954e9bc1ac212dd9
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-01-20 12:50:19 +0100

    28# Home Controller

--- LibraryManagementCourse/Controllers/HomeController.cs
+++ LibraryManagementCourse/Controllers/HomeController.cs
@@ -5,33 +5,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LibraryManagementCourse.Models;
+using LibraryManagementCourse.Data.Interfaces;
+using LibraryManagementCourse.ViewModel;
 
 namespace LibraryManagementCourse.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IBookRepository _bookRepository;
+        private readonly IAuthorRepository _authorRepository;
+        private readonly ICustomerRepository _customerRepository;
+
+        public HomeController(IBookRepository bookRepository,
+                              IAuthorRepository authorRepository,
+                              ICustomerRepository customerRepository)
+        {
+            _bookRepository = bookRepository;
+            _authorRepository = authorRepository;
+            _customerRepository = customerRepository;
+        }
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // create home view model
+            var homeVM = new HomeViewModel()
+            {
+                AuthorCount = _authorRepository.Count(x => true),
+                CustomerCount = _customerRepository.Count(x=>true),
+                BookCount = _bookRepository.Count(x => true),
+                LendBookCount= _bookRepository.Count(x => x.Borrower != null)
+            };
+            // call view
+            return View(homeVM);
         }
     }
 }
---
+++ LibraryManagementCourse/ViewModel/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementCourse.ViewModel
+{
+    public class HomeViewModel
+    {
+        public int CustomerCount { get; set; }
+        public int AuthorCount { get; set; }
+        public int BookCount { get; set; }
+        public int LendBookCount { get; set; }
+    }
+}
